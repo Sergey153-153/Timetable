3aa510f4dd8695c28cf726e5cacf2cda0b9617e0
Sergey <sergeylokt153@gmail.com>
2025-11-21 23:38:45 +0300

    расширение пары начало

---
+++ SQLiteProject/MoveForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteProject
+{
+    public partial class MoveForm : Form
+    {
+        private int lessonId;
+        // Новый конструктор с аргументом
+        public MoveForm(int currentLessonId)
+        {
+            InitializeComponent();
+            lessonId = currentLessonId;
+
+            // здесь можно сразу что-то сделать с lessonId, если нужно
+            // например, загрузить данные урока
+        }
+    }
+}
