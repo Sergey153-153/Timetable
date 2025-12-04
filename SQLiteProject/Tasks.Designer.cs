ac0b2dc3e59b2e3d215fab6408316859c6e6b5d3
dashikozh <dashikozh@gmail.com>
2025-12-04 13:07:20 +0300

    Добавлены кнопки и область для размещения заданий

--- SQLiteProject/Tasks.Designer.cs
+++ SQLiteProject/Tasks.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonSchelude = new System.Windows.Forms.Button();
+            this.buttonSetting = new System.Windows.Forms.Button();
+            this.buttonAddTask = new System.Windows.Forms.Button();
+            this.panelTasks = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonSchelude
+            // 
+            this.buttonSchelude.Location = new System.Drawing.Point(51, 828);
+            this.buttonSchelude.Name = "buttonSchelude";
+            this.buttonSchelude.Size = new System.Drawing.Size(118, 89);
+            this.buttonSchelude.TabIndex = 1;
+            this.buttonSchelude.Text = "Расписание";
+            this.buttonSchelude.UseVisualStyleBackColor = true;
+            // 
+            // buttonSetting
+            // 
+            this.buttonSetting.Location = new System.Drawing.Point(366, 828);
+            this.buttonSetting.Name = "buttonSetting";
+            this.buttonSetting.Size = new System.Drawing.Size(118, 89);
+            this.buttonSetting.TabIndex = 2;
+            this.buttonSetting.Text = "Настройки";
+            this.buttonSetting.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddTask
+            // 
+            this.buttonAddTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAddTask.Location = new System.Drawing.Point(228, 828);
+            this.buttonAddTask.Name = "buttonAddTask";
+            this.buttonAddTask.Size = new System.Drawing.Size(84, 89);
+            this.buttonAddTask.TabIndex = 3;
+            this.buttonAddTask.Text = "+";
+            this.buttonAddTask.UseVisualStyleBackColor = true;
+            // 
+            // panelTasks
+            // 
+            this.panelTasks.Location = new System.Drawing.Point(9, 11);
+            this.panelTasks.Name = "panelTasks";
+            this.panelTasks.Size = new System.Drawing.Size(518, 789);
+            this.panelTasks.TabIndex = 4;
+            // 
+            // Tasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(538, 937);
+            this.Controls.Add(this.panelTasks);
+            this.Controls.Add(this.buttonAddTask);
+            this.Controls.Add(this.buttonSetting);
+            this.Controls.Add(this.buttonSchelude);
+            this.Name = "Tasks";
             this.Text = "Tasks";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button buttonSchelude;
+        private System.Windows.Forms.Button buttonSetting;
+        private System.Windows.Forms.Button buttonAddTask;
+        private System.Windows.Forms.Panel panelTasks;
     }
 }
